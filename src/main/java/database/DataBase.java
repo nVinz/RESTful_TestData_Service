f01401281b639cfeaa6ca35330df00b03f4ad67e
VChizhov <vladislav.chizhov@aplana.com>
2019-08-31 15:28:49 +0300

    upd Database

--- src/main/java/database/DataBase.java
+++ src/main/java/database/DataBase.java
@@ -4,32 +4,42 @@ package database;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.data.Table;
+import webservice.AppProperties;
 
+import java.io.IOException;
 import java.sql.DriverManager;
+import java.util.Objects;
 import java.util.Properties;
 
 public class DataBase {
 
     static Sql2o connection;
     public Table table;
+    Properties properties = new Properties();
+
+    public DataBase() {
+        try {
+            properties.load(Objects.requireNonNull(AppProperties.class.getClassLoader().getResourceAsStream("app.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void connectToBD() {
-        Properties properties = new Properties();
         String server, port, login, password, service;
-
         server = properties.get("db.host").toString();
         port = properties.get("db.port").toString();
         login = properties.get("db.username").toString();
         password = properties.get("db.password").toString();
         service = properties.get("db.service").toString();
 
-        connection = new Sql2o("jdbc:postgresql://localhost:5432/example", "postgres", "postgres");
+        connection = new Sql2o(String.format("jdbc:postgresql://%s:%s/%s", server, port, service), login, password);
     }
 
-    public void executeQuery(String request){
+    public Table executeQuery(String request) {
         Connection con = connection.open();
         table = con.createQuery(request).executeAndFetchTable();
-
+        return table;
     }
 
 }
--- src/main/java/webservice/controller/MainController.java
+++ src/main/java/webservice/controller/MainController.java
@@ -1,9 +1,15 @@
 package webservice.controller;
 
+import database.DataBase;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.sql2o.data.Column;
+import org.sql2o.data.Row;
+import org.sql2o.data.Table;
+
+import java.sql.ResultSet;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -14,6 +20,17 @@ public class MainController {
     @GetMapping(value = "/test")
     @ResponseBody
     public String testSys(){
-        return "worked";
+        String result = "";
+
+        DataBase db = new DataBase();
+        db.connectToBD();
+        Table res = db.executeQuery("select * from login");
+        for (Row row : res.rows()) {
+            for (Column column : res.columns()) {
+                result += row.getString(column.getName());
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
