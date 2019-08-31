05d566c62d05cfc7b702d6fbf85bd08b4bfda84d
Zhanna <dendy2@yandex.ru>
2019-08-31 12:13:31 +0300

    [upd run appl

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,26 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.security.AlgorithmParameterGenerator;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
+    private static ConfigurableApplicationContext context = null;
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        AppProperties.getInstance().addApplicationArgs(args);
+
+        SpringApplication application = new SpringApplication(Application.class);
+
+        Properties properties = new Properties();
+        properties.put("server.port", AppProperties.getInstance().getPort());
+        properties.put("server.address", AppProperties.getInstance().getHost());
+        application.setDefaultProperties(properties);
+
+
+        context = application.run(args);
     }
 }
