f579e781863a9c030aea027d03dc0b59d4a07bc6
VChizhov <vladislav.chizhov@aplana.com>
2019-09-01 12:37:11 +0300

    new function getValues

--- src/main/java/webservice/controller/MainController.java
+++ src/main/java/webservice/controller/MainController.java
@@ -1,6 +1,7 @@
 package webservice.controller;
 
 import database.DataBase;
+import generations.Generating;
 import org.postgresql.util.PSQLException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,8 @@ import org.sql2o.data.Row;
 import org.sql2o.data.Table;
 
 import java.sql.ResultSet;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -158,14 +161,134 @@ public class MainController {
 
     private boolean isValueValidByMask(String value, String mask) {
         if (mask.contains("regexp")){
-
+            return value.matches(mask.replace("regexp", ""));
         }
         else if (mask.contains("script")) {
-
+            return true;
         }
         else {
-
+            if (mask.contains("chr")) return value.matches("\\w+");
+            if (mask.contains("int")) return value.matches("\\d+");
+            if (mask.contains("bool")) return value.matches("(true)|(false)");
+            if (mask.contains("date")) return value.matches("\\d{2}\\.\\d{2}.\\d{4}");
+            return true;
         }
-        return true;
+    }
+
+    @RequestMapping(value = "/generateValues{project}{tableName}{count}", method = GET, produces = "application/json")
+    @ResponseBody
+    public Collection generateValues(@RequestParam("project") String project, @RequestParam("tableName") String tableName, @RequestParam("count") int count){
+        String templateToGetMasks = "SELECT \"column\", \"mask\" FROM masks JOIN tables ON masks.\"id\" = tables.\"id\" WHERE \"name\" = '%s' AND \"project\" = '%s' AND \"column\" in (SELECT column_name FROM information_schema.columns WHERE table_name = '%s')";
+        String templateToInsert = "INSERT INTO %s(%s) VALUES (%s)";
+        String requestToInsert = "";
+        String requestColumns = "";
+        String requestToGetMasks = "";
+        Table res = null;
+
+        requestToGetMasks = String.format(templateToGetMasks, tableName, project, tableName.toLowerCase());
+
+        DataBase db = new DataBase();
+        db.connectToBD();
+        if (db.isTableExists(tableName)) {
+            try{
+                res = db.executeQuery(requestToGetMasks);
+                for (int i = 0; i < res.rows().size(); i++) {
+                    if (i != 0) requestColumns += ", ";
+                    requestColumns += String.format("\"%s\"", res.rows().get(i).getString("column").toLowerCase());
+                }
+            }
+            catch (PSQLException e){
+                System.out.println("Error occured while request " + e.getMessage());
+                return Collections.singleton("Error occured while request " + e.getMessage());
+            }
+        }
+        else return Collections.singleton(String.format("Table with name %s doesn't exist", tableName));
+
+        for (int i = 0; i < count; i++) {
+            if (!requestToInsert.equals("")) requestToInsert += "), (";
+            for (int j = 0; j < res.rows().size(); j++) {
+                if (j != 0) requestToInsert += ", ";
+                String mask = res.rows().get(j).getString("mask");
+                String value = generateValueByMask(mask);
+
+                requestToInsert += String.format("'%s'", value);
+            }
+        }
+
+        Collection col = Collections.singleton(requestToInsert.split("\\), \\("));
+
+        requestToInsert = String.format(templateToInsert, tableName, requestColumns, requestToInsert);
+
+        try{
+            db.executeQueryWithoutResult(requestToInsert);
+        }
+        catch (PSQLException e){
+            System.out.println("Error occured while request " + e.getMessage());
+
+            return Collections.singleton("Error occured while request " + e.getMessage());
+        }
+        return col;
+    }
+
+    private String generateValueByMask(String mask){
+        if (mask.contains("regexp")){
+            return Generating.generateByRegEXP(mask.replace("regexp", ""));
+        }
+        else if (mask.contains("script")) {
+            return "";
+        }
+        else {
+            if (mask.contains("chr")) return Generating.generateCharSet();
+            if (mask.contains("int")) return String.valueOf(Generating.generateNumeric());
+            if (mask.contains("bool")) return String.valueOf(Generating.generateBool());
+            if (mask.contains("date")) return String.valueOf(Generating.randomDate());
+            return "";
+        }
+    }
+
+    @RequestMapping(value = "/getValues{project}{tableName}{count}", method = GET, produces = "application/json")
+    @ResponseBody
+    public Collection getValues(@RequestParam("project") String project, @RequestParam("tableName") String tableName, @RequestParam("count") int count){
+        String templateToGetValues = "SELECT * FROM %s WHERE \"blocked\" IS NULL OR \"blocked\" > NOW() LIMIT %s";
+        String templateToUpdate = "";
+        String requestToGetValues = "";
+        String requestToUpdate = "";
+
+        Table res = null;
+
+        requestToGetValues = String.format(templateToGetValues, tableName, count);
+
+        DataBase db = new DataBase();
+        db.connectToBD();
+        if (db.isTableExists(tableName)) {
+            try{
+                res = db.executeQuery(requestToGetValues);
+            }
+            catch (PSQLException e){
+                System.out.println("Error occured while request " + e.getMessage());
+                return Collections.singleton("Error occured while request " + e.getMessage());
+            }
+        }
+        else return Collections.singleton(String.format("Table with name %s doesn't exist", tableName));
+
+        String rows[][] = new String[res.rows().size()][];
+        for (int i = 0; i < res.rows().size(); i++) {
+            String values[] = new String[res.columns().size()];
+            for (int j = 0; j < res.columns().size(); j++) {
+                String value = res.rows().get(i).getString(j);
+                values[j] = value;
+            }
+            rows[i] = values;
+        }
+
+//        try{
+//            db.executeQueryWithoutResult(requestToInsert);
+//        }
+//        catch (PSQLException e){
+//            System.out.println("Error occured while request " + e.getMessage());
+//
+//            return Collections.singleton("Error occured while request " + e.getMessage());
+//        }
+        return Collections.singleton(rows);
     }
 }
