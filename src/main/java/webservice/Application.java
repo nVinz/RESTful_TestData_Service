e326fba2450cac75572b7de6f9a9ed72325b1fab
VChizhov <vladislav.chizhov@aplana.com>
2019-09-01 14:36:28 +0300

    upd Application

--- src/main/java/webservice/Application.java
+++ src/main/java/webservice/Application.java
@@ -1,5 +1,6 @@
 package webservice;
 
+import database.DataBase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        DataBase db = new DataBase();
+        db.connectToBD();
+        db.setDBEnvironment();
+
         SpringApplication.run(Application.class, args);
     }
 }
