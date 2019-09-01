6c28af875743dc4994bce55674d83abb60c0a179
Zhanna <dendy2@yandex.ru>
2019-09-01 14:25:35 +0300

    [add] index

--- src/main/java/webservice/Application.java
+++ src/main/java/webservice/Application.java
@@ -2,28 +2,11 @@ package webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-
-import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
-//    private static ConfigurableApplicationContext context = null;
-
     public static void main(String[] args) {
-//        AppProperties.getInstance().addApplicationArgs(args);
-
-//        SpringApplication application = new SpringApplication(Application.class);
         SpringApplication.run(Application.class, args);
-//
-//        Properties properties = new Properties();
-//        properties.put("server.port", AppProperties.getInstance().getPort());
-//        properties.put("server.address", AppProperties.getInstance().getHost());
-//        application.setDefaultProperties(properties);
-
-
-//        context = application.run(args);
     }
 }
