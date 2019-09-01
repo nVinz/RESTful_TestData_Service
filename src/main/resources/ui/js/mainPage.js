2abcfa2e45dca68f7a128be3dcd719d4ff92da3b
Zhanna <dendy2@yandex.ru>
2019-09-01 10:41:19 +0300

    [upd] mainPage js

--- src/main/resources/ui/js/mainPage.js
+++ src/main/resources/ui/js/mainPage.js
@@ -2,20 +2,20 @@
 
 
 
+$('#updateTable').click(function downloadTable() {
+    if (userHasAccess()) return
+      $.ajax({url: "/getFilteredHTMLTable",
+       data:{
+            'tableName': 'tables',
+            'columnName': 'name',
+            'filter': ${'#filterInput'}.getContext(),
+            'tableStyle': ' class=\"table\"'
+       },
+       success: function(result){
+            $('#mainTable').text(result)
+          }});
+    })
 
-function downloadTable() {
-if (userHasAccess()) return
-  $.ajax({url: "/getFilteredHTMLTable",
-   data:{
-        'tableName': 'tables',
-        'columnName': 'name',
-        'filter': ${'#filterInput'}.getContext(),
-        'tableStyle': ' class=\"table\"'
-   },
-   success: function(result){
-        $('#mainTable').text(result)
-      }});
-}
 function userHasAccess(){
     return true;
 }
