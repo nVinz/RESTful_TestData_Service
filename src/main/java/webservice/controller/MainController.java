d311c358129920602f7a56e9127bbaa709cebc99
VChizhov <vladislav.chizhov@aplana.com>
2019-09-01 10:20:08 +0300

    change in db logic

--- src/main/java/webservice/controller/MainController.java
+++ src/main/java/webservice/controller/MainController.java
@@ -40,47 +40,62 @@ public class MainController {
         return result;
     }
 
-    @RequestMapping(value = "/createTable{project}{tableName}{columns}{masks}", method = GET)
+    @RequestMapping(value = "/createTable{project}{tableName}{description}{columns}{masks}", method = GET)
     @ResponseBody
-    public String createTable(@RequestParam("project") String project, @RequestParam("tableName") String tableName, @RequestParam("columns") String[] columns, @RequestParam("masks") String[] masks){
-        String templateToInsert = "INSERT INTO masks VALUES %s";
+    public String createTable(@RequestParam("project") String project, @RequestParam("tableName") String tableName, @RequestParam("description") String description, @RequestParam("columns") String[] columns, @RequestParam("masks") String[] masks){
+        String templateToInsertMasks = "INSERT INTO masks(\"column\", \"mask\", \"id\") VALUES %s";
+        String templateToInsertTables = "INSERT INTO tables(\"id\", \"name\", \"project\", \"description\") VALUES (coalesce((SELECT \"id\"+1 FROM tables ORDER BY \"id\" DESC LIMIT 1), 1), '%s', '%s', '%s') RETURNING \"id\"";
         String templateToCreate = "CREATE TABLE %s (%s release varchar(255), blocked date, reused boolean)";
-        String requestToInsert = "";
+        String requestToInsertMasks = "";
+        String requestToInsertTables = "";
         String requestToCreate = "";
+        String id;
+
+        requestToInsertTables = String.format(templateToInsertTables, tableName, project, description);
+
+        DataBase db = new DataBase();
+        db.connectToBD();
+        if (!db.isTableExists(tableName)) {
+            try {
+                Table res = db.executeQuery(requestToInsertTables);
+                id = res.rows().get(0).getString(0);
+            }
+            catch (PSQLException e) {
+                System.out.println("Error occured while request " + e.getMessage());
+                return "Error occured while request " + e.getMessage();
+            }
+        }
+        else return String.format("Table with name %s has already existed", tableName);
+
         for (int i = 0; i < columns.length; i++) {
             String column = columns[i].toLowerCase();
             String mask = masks[i];
 
             requestToCreate += column + " varchar(255), ";
 
-            if (!requestToInsert.equals("")) requestToInsert += ", ";
-            requestToInsert += String.format("('%s', '%s', '%s', '%s')", column, mask, tableName, project);
+            if (!requestToInsertMasks.equals("")) requestToInsertMasks += ", ";
+            requestToInsertMasks += String.format("('%s', '%s', '%s')", column, mask, id);
         }
 
-        requestToInsert = String.format(templateToInsert, requestToInsert);
+        requestToInsertMasks = String.format(templateToInsertMasks, requestToInsertMasks);
         requestToCreate = String.format(templateToCreate, tableName, requestToCreate);
 
-        DataBase db = new DataBase();
-        db.connectToBD();
-        if (!db.isTableExists(tableName)) {
-            try {
-                db.executeQueryWithoutResult(requestToInsert);
-                db.executeQueryWithoutResult(requestToCreate);
-            }
-            catch (PSQLException e) {
-                System.out.println("Error occured while request " + e.getMessage());
-                return "Error occured while request " + e.getMessage();
-            }
-
-            return "Table was successfully created";
+        try {
+            db.executeQueryWithoutResult(requestToInsertMasks);
+            db.executeQueryWithoutResult(requestToCreate);
         }
-        return String.format("Table with name %s has already existed", tableName);
+        catch (PSQLException e) {
+            System.out.println("Error occured while request " + e.getMessage());
+            return "Error occured while request " + e.getMessage();
+        }
+
+        return "Table was successfully created";
     }
 
     @RequestMapping(value = "/insertValues{project}{tableName}{columns}{values}", method = GET)
     @ResponseBody
     public String insertValues(@RequestParam("project") String project, @RequestParam("tableName") String tableName, @RequestParam("columns") String[] columns, @RequestParam("values") String values){
-        String templateToGetMasks = "SELECT column, mask FROM masks WHERE table = '%s' AND project = '%s' AND column in (%s)";
+        String templateToGetMasks = "SELECT \"column\", \"mask\" FROM masks JOIN tables ON masks.\"id\" = tables.\"id\" WHERE \"name\" = '%s' AND \"project\" = '%s' AND \"column\" in (%s)";
         String templateToInsert = "INSERT INTO %s(%s) VALUES (%s)";
         String requestToInsert = "";
         String requestColumns = "";
@@ -93,7 +108,7 @@ public class MainController {
             requestColumns += String.format("\"%s\"", columns[i].toLowerCase());
         }
 
-        requestToGetMasks = String.format(templateToGetMasks, tableName, project, requestColumns);
+        requestToGetMasks = String.format(templateToGetMasks, tableName, project, requestColumns.replace("\"", "'"));
 
         DataBase db = new DataBase();
         db.connectToBD();
@@ -114,8 +129,7 @@ public class MainController {
             for (int j = 0; j < splittedRow.length; j++) {
                 if (j != 0) requestToInsert += ", ";
                 String mask = getMaskByColumnName(columns[j], res);
-                if (mask.equals("")) return String.format("Column %s wasn't found in masks", columns[j]);
-                else{
+                if (!mask.equals("")) {
                     if (!isValueValidByMask(splittedRow[j], mask)) return String.format("Value %s isn't valid by mask %s", splittedRow[j], mask);
                 }
                 requestToInsert += String.format("'%s'", splittedRow[j]);
@@ -143,10 +157,10 @@ public class MainController {
     }
 
     private boolean isValueValidByMask(String value, String mask) {
-        if (mask.contains("regexp:")){
+        if (mask.contains("regexp")){
 
         }
-        else if (mask.contains("script:")) {
+        else if (mask.contains("script")) {
 
         }
         else {
